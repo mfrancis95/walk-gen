9c0ac993d56addd76b47552f5a89d99cfa5d274b
mfrancis95 <mikefrancis95@gmail.com>
2016-03-02 13:45:22 -0500

    Add a maximum walk depth parameter

--- walk-gen.js
+++ walk-gen.js
@@ -1,14 +1,20 @@
 var fs = require("fs");
 var path = require("path");
 
-function* walk(directory) {
-    for (var file of fs.readdirSync(directory)) {
-        file = path.join(directory, file);
-        if (fs.statSync(file).isDirectory()) {
-            yield* walk(file);
-        }
-        else {
-            yield file;
+function* walk(directory, maxDepth) {
+    yield* walkHelper(directory, maxDepth, 0);
+}
+
+function* walkHelper(directory, maxDepth, currentDepth) {
+    if (currentDepth <= maxDepth) {
+        for (var file of fs.readdirSync(directory)) {
+            file = path.join(directory, file);
+            if (fs.statSync(file).isDirectory()) {
+                yield* walkHelper(file, maxDepth, currentDepth + 1);
+            }
+            else {
+                yield file;
+            }
         }
     }
 }
