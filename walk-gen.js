04419f1bc0dd436e4ed8309db41e19cc0f6a0456
mfrancis95 <mikefrancis95@gmail.com>
2016-03-14 15:19:17 -0400

    Add optional yieldDirectories argument

--- walk-gen.js
+++ walk-gen.js
@@ -1,15 +1,18 @@
 var fs = require("fs");
 var path = require("path");
 
-function* walk(directory, maxDepth) {
-    yield* walkHelper(directory, maxDepth, 0);
+function* walk(directory, maxDepth, yieldDirectories) {
+    yield* walkHelper(directory, maxDepth, 0, yieldDirectories);
 }
 
-function* walkHelper(directory, maxDepth, currentDepth) {
+function* walkHelper(directory, maxDepth, currentDepth, yieldDirectories) {
     if (currentDepth <= maxDepth) {
         for (var file of fs.readdirSync(directory)) {
             file = path.join(directory, file);
             if (fs.statSync(file).isDirectory()) {
+                if (yieldDirectories) {
+                    yield file;
+                }
                 yield* walkHelper(file, maxDepth, currentDepth + 1);
             }
             else {
