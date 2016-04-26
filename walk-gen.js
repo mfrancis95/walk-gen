c4ad84ebd2f382de8c7cc48c62b19b0125a443b9
mfrancis95 <mikefrancis95@gmail.com>
2016-04-26 08:38:13 -0400

    Add optional followSymbolicLinks argument

--- walk-gen.js
+++ walk-gen.js
@@ -1,19 +1,26 @@
 var fs = require("fs");
 var path = require("path");
 
-function* walk(directory, maxDepth, yieldDirectories) {
-    yield* walkHelper(directory, maxDepth, yieldDirectories, 0);
+function* walk(directory, maxDepth, yieldDirectories, followSymbolicLinks) {
+    yield* walkHelper(directory, maxDepth, yieldDirectories, followSymbolicLinks, 0);
 }
 
-function* walkHelper(directory, maxDepth, yieldDirectories, currentDepth) {
+function* walkHelper(directory, maxDepth, yieldDirectories, followSymbolicLinks, currentDepth) {
     if (currentDepth <= maxDepth) {
         for (var file of fs.readdirSync(directory)) {
             file = path.join(directory, file);
-            if (fs.statSync(file).isDirectory()) {
+            var stats = fs.lstatSync(file);
+            if (stats.isSymbolicLink()) {
+                yield file;
+                if (followSymbolicLinks) {
+                    yield* walkHelper(file, maxDepth, yieldDirectories, followSymbolicLinks, currentDepth + 1);
+                }
+            }
+            else if (stats.isDirectory()) {
                 if (yieldDirectories) {
                     yield file;
                 }
-                yield* walkHelper(file, maxDepth, yieldDirectories, currentDepth + 1);
+                yield* walkHelper(file, maxDepth, yieldDirectories, followSymbolicLinks, currentDepth + 1);
             }
             else {
                 yield file;
